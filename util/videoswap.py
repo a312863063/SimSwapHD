7f80295fd312ee3927df938c9506eb892332bd48
seeprettyface.com <38420957+a312863063@users.noreply.github.com>
2021-09-29 16:39:48 +0800

    Update videoswap.py

--- util/videoswap.py
+++ util/videoswap.py
@@ -78,7 +78,7 @@ def video_swap(video_path, id_vetor, swap_model, detect_model, save_path, temp_r
 
                     frame_align_crop_tenor = _totensor(cv2.cvtColor(frame_align_crop,cv2.COLOR_BGR2RGB))[None,...].cuda()
 
-                    swap_result = swap_model(None, frame_align_crop_tenor, id_vetor, None, True)[0]
+                    swap_result = swap_model(None, spNorm(frame_align_crop_tenor), id_vetor, None, True)[0]
                     swap_result_list.append(swap_result)
                     frame_align_crop_tenor_list.append(frame_align_crop_tenor)
 
